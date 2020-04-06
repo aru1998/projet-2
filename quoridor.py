36fee85fc43a737525122a8e6220b5a3ec4fb989
Ange raissa Uwase <ange@Anges-MacBook-Air.local>
2020-04-06 14:39:00 -0400

    essaie 3 commit

--- quoridor.py
+++ quoridor.py
@@ -1,5 +1,5 @@
-"""Quoridor - module quoridor"""
-from copy import deepcopy
+"""module quoridor"""
+import networkx as nx
 
 
 def construire_graphe(joueurs, murs_horizontaux, murs_verticaux):
@@ -125,84 +125,6 @@ class Quoridor:
                 raise QuoridorError("Un des murs horizontaux et un des murs verticaux se "
                                     "chevauchent")
 
-    def __init__(self, joueurs, murs=None):
-        """
-        Initialiser une partie de Quoridor avec les joueurs et les murs spécifiés,
-        en s'assurant de faire une copie profonde de tout ce qui a besoin d'être copié.
-        :param joueurs: un itérable de deux joueurs dont le premier est toujours celui qui
-        débute la partie. Un joueur est soit une chaîne de caractères soit un dictionnaire.
-        Dans le cas d'une chaîne, il s'agit du nom du joueur. Selon le rang du joueur dans
-        l'itérable, sa position est soit (5,1) soit (5,9), et chaque joueur peut initialement
-        placer 10 murs. Dans le cas où l'argument est un dictionnaire, celui-ci doit contenir
-        une clé 'nom' identifiant le joueur, une clé 'murs' spécifiant le nombre de murs qu'il
-        peut encore placer, et une clé 'pos' qui spécifie sa position (x, y) actuelle.
-        :param murs: un dictionnaire contenant une clé 'horizontaux' associée à la liste des
-        positions (x, y) des murs horizontaux, et une clé 'verticaux' associée à la liste des
-        positions (x, y) des murs verticaux. Par défaut, il n'y a aucun mur placé sur le jeu.
-        :raises QuoridorError: si l'argument 'joueurs' n'est pas itérable.
-        :raises QuoridorError: si l'itérable de joueurs en contient plus de deux.
-        :raises QuoridorError: si le nombre de murs qu'un joueur peut placer est >10, ou négatif.
-        :raises QuoridorError: si la position d'un joueur est invalide.
-        :raises QuoridorError: si l'argument 'murs' n'est pas un dictionnaire lorsque présent.
-        :raises QuoridorError: si le total des murs placés et plaçables n'est pas égal à 20.
-        :raises QuoridorError: si la position d'un mur est invalide.
-        """
-        try:
-            iter(joueurs)
-        except TypeError:
-            raise QuoridorError("L'argument 'joueurs' n'est pas itérable")
-
-        if len(joueurs) != 2:
-            raise QuoridorError("Il doit uniquement y avoir 2 joueurs")
-
-        nb_murs = 0
-        self.etat = {"joueurs": [], "murs": None}
-        self.type_coup = ""
-        self.pos_coup = None
-
-        for i, joueur in enumerate(joueurs):
-            if isinstance(joueur, dict):
-                if not (isinstance(joueur["murs"], int) and 0 <= joueur["murs"] <= 10):
-                    raise QuoridorError("Le nombre de murs qu'un joueur peut placer est >10, "
-                                        "négatif, ou invalide")
-
-                if not self.pos_joueur_valide(joueur["pos"]):
-                    raise QuoridorError("La position d'un des joueurs est invalide")
-
-                nb_murs += joueur["murs"]
-                self.etat["joueurs"].append(deepcopy(joueur))
-            else:
-                self.etat["joueurs"].append({
-                    "nom": joueur,
-                    "murs": 10,
-                    "pos": (5, 1 if i == 0 else 9)
-                })
-
-        if murs is not None:
-            if not isinstance(murs, dict):
-                raise QuoridorError("L'argument 'murs' n'est pas un dictionnaire")
-
-            murs_h, murs_v = murs["horizontaux"], murs["verticaux"]
-
-            self.valider_murs(murs_h, murs_v)
-
-            graphe = construire_graphe(
-                [joueur["pos"] for joueur in self.etat["joueurs"]],
-                murs_h,
-                murs_v
-            )
-
-            if any(not nx.has_path(graphe, tuple(joueur["pos"]), f'B{i+1}')
-                   for i, joueur in enumerate(joueurs)):
-                raise QuoridorError("Un des joueurs est emprisonné par des murs")
-
-            nb_murs += len(murs_h) + len(murs_v)
-
-        if nb_murs != 20:
-            raise QuoridorError("Le total des murs placés et plaçables n'est pas égal à 20")
-
-        self.etat["murs"] = {"horizontaux": [], "verticaux": []} if murs is None else deepcopy(murs)
-
     def __str__(self):
         """
         Produire la représentation en art ascii correspondant à l'état actuel de la partie.
@@ -281,34 +203,6 @@ class Quoridor:
             raise QuoridorError("La position est invalide pour l'état actuel du jeu")
 
         dict_joueur["pos"] = position
-
-    def état_partie(self):
-        """
-        Produire l'état actuel de la partie.
-        :returns: une copie de l'état actuel du jeu sous la forme d'un dictionnaire:
-        {
-            'joueurs': [
-                {'nom': nom1, 'murs': n1, 'pos': (x1, y1)},
-                {'nom': nom2, 'murs': n2, 'pos': (x2, y2)},
-            ],
-            'murs': {
-                'horizontaux': [...],
-                'verticaux': [...],
-            }
-        }
-        où la clé 'nom' d'un joueur est associée à son nom, la clé 'murs' est associée
-        au nombre de murs qu'il peut encore placer sur ce damier, et la clé 'pos' est
-        associée à sa position sur le damier. Une position est représentée par un tuple
-        de deux coordonnées x et y, où 1<=x<=9 et 1<=y<=9.
-        Les murs actuellement placés sur le damier sont énumérés dans deux listes de
-        positions (x, y). Les murs ont toujours une longueur de 2 cases et leur position
-        est relative à leur coin supérieur gauche lorsque horizontal et inférieur droit
-        lorsque vertical.
-        Par convention, un mur horizontal sensitue entre les lignes y-1 et y, et bloque
-        les colonnes x et x+1. De même, un mur vertical se situe entre les colonnes x-1
-        et x, et bloque les lignes y et y+1.
-        """
-        return deepcopy(self.etat)
 
     def jouer_coup(self, joueur):
         """
